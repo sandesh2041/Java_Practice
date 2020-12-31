3c659ee2486d6dfc4c61ec9d81fb52df1d8f66c6
sandesh2041 <sandeshmas@gmail.com>
2020-12-31 14:58:10 -0700

    Comment added again

--- src/com/sandesh/SquareDigit.java
+++ src/com/sandesh/SquareDigit.java
@@ -11,7 +11,7 @@ public class SquareDigit {
      public int squareDigits(int n) {
 
         String combine = "";
-        String numStr = Integer.toString(n);
+        String numStr = Integer.toString(n); // Convert number to string
 
         for(int i = 0; i< numStr.length(); i++){
             int square = Character.getNumericValue(numStr.charAt(i)) * Character.getNumericValue(numStr.charAt(i));
